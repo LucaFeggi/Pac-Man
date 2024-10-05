ca5d28e962d6aefa554c2e0e36c2b8c4b1d62d56
LucaFeggi <81522854+LucaFeggi@users.noreply.github.com>
2024-10-05 11:53:58 +0200

    Update GameInputs.cpp

--- Source/SourceFiles/GameInputs.cpp
+++ Source/SourceFiles/GameInputs.cpp
@@ -5,23 +5,18 @@ GameInputs::GameInputs(bool* Quit, bool* IsToRestart, Directions* Mover) :
 	IsToRestart(IsToRestart),
 	Mover(Mover),
 	Event(),
-	MyJoysticks(),
+	MyJoystick(),
 	JoystickDeadZone(8000),
 	IsFingerDown(false),
 	FingerDownCoords{WindowWidth/2, WindowHeight/2}
 {
-	for (int i = 0; i < SDL_NumJoysticks(); i++) {
-		MyJoysticks.push_back(SDL_JoystickOpen(i));
+	if (SDL_NumJoysticks() > 0) {
+		SDL_JoystickOpen(0);
 	}
 }
 
 GameInputs::~GameInputs() {
-	for (int i = 0; i < MyJoysticks.size(); i++) {
-		SDL_JoystickClose(MyJoysticks.at(i));
-	}
-	while (!MyJoysticks.empty()) {
-		MyJoysticks.erase(MyJoysticks.begin());
-	}
+	SDL_JoystickClose(MyJoystick);
 	Quit = nullptr;
 	IsToRestart = nullptr;
 	Mover = nullptr;
@@ -35,7 +30,7 @@ void GameInputs::Get() {
 		}
 		Keyboard();
 		Buttons();
-		Joysticks();
+		Joystick();
 		Touch();
 	}
 }
@@ -67,60 +62,56 @@ void GameInputs::Keyboard() {
 	}
 }
 
-void GameInputs::Buttons() {
-	if (Event.type == SDL_JOYBUTTONDOWN) {
-		for (int i = 0; i < MyJoysticks.size(); i++) {
-			if (Event.jbutton.which == i) {
-				switch (Event.jbutton.button) {
-				case 0:
-					*IsToRestart = true;
-					break;
-				case 1:
-					Mover[1] = Directions::Up;
-					break;
-				case 3:
+void GameInputs::Joystick() {
+	if (Event.type == SDL_JOYAXISMOTION) {
+		if (Event.jaxis.which == SDL_JoystickGetDeviceInstanceID(0)) {
+			switch (Event.jaxis.axis) {
+			case 0:
+				if (Event.jaxis.value < -JoystickDeadZone) {
 					Mover[1] = Directions::Left;
 					break;
-				case 4:
-					Mover[1] = Directions::Down;
-					break;
-				case 5:
+				}
+				if (Event.jaxis.value > JoystickDeadZone) {
 					Mover[1] = Directions::Right;
-					break;
-				default:
+				}
+				break;
+			case 1:
+				if (Event.jaxis.value < -JoystickDeadZone) {
+					Mover[1] = Directions::Up;
 					break;
 				}
+				if (Event.jaxis.value > JoystickDeadZone) {
+					Mover[1] = Directions::Down;
+				}
+				break;
+			default:
+				break;
 			}
 		}
 	}
 }
 
-void GameInputs::Joysticks() {
-	if (Event.type == SDL_JOYAXISMOTION) {
-		for (int i = 0; i < MyJoysticks.size(); i++) {
-			if (Event.jaxis.which == i) {
-				switch (Event.jaxis.axis) {
-				case 0:
-					if (Event.jaxis.value < -JoystickDeadZone) {
-						Mover[1] = Directions::Left;
-						break;
-					}
-					if (Event.jaxis.value > JoystickDeadZone) {
-						Mover[1] = Directions::Right;
-					}
-					break;
-				case 1:
-					if (Event.jaxis.value < -JoystickDeadZone) {
-						Mover[1] = Directions::Up;
-						break;
-					}
-					if (Event.jaxis.value > JoystickDeadZone) {
-						Mover[1] = Directions::Down;
-					}
-					break;
-				default:
-					break;
-				}
+void GameInputs::Buttons() {
+	if (Event.type == SDL_JOYBUTTONDOWN) {
+		if (Event.jbutton.which == SDL_JoystickGetDeviceInstanceID(0)) {
+			switch (Event.jbutton.button) {
+			case 0:
+				Mover[1] = Directions::Down;
+				break;
+			case 1:
+				Mover[1] = Directions::Right;
+				break;
+			case 2:
+				Mover[1] = Directions::Left;
+				break;
+			case 3:
+				Mover[1] = Directions::Up;
+				break;
+			case 4:
+				*IsToRestart = true;
+				break;
+			default:
+				break;
 			}
 		}
 	}
